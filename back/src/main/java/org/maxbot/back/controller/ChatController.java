adf3672d993cf0f30bac3392cafa85f972402a65
maximedrouault <maximedrouault@gmail.com>
2025-10-02 08:44:41 +0200

    refactor: update application configuration for Ollama integration and adjust chat controller

--- back/src/main/java/org/maxbot/back/controller/ChatController.java
+++ back/src/main/java/org/maxbot/back/controller/ChatController.java
@@ -1,7 +1,9 @@
 package org.maxbot.back.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +15,15 @@ public class ChatController {
         this.chatClient = chatClientBuilder.build();
     }
 
+    @Value("${PROMPT_TEMPLATE}")
+    private String SYSTEM_TEMPLATE;
+
 
     @GetMapping("/chat")
-    public String chat(String userInput) {
+    public Object chat(@RequestBody String userInput) {
 
         return this.chatClient.prompt()
+                .system(SYSTEM_TEMPLATE)
                 .user(userInput)
                 .call()
                 .content();
