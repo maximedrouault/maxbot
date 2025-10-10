d199e3b9e0687d1ee593dcda860e17b4495c35fa
maximedrouault <maximedrouault@gmail.com>
2025-10-10 17:42:31 +0200

    add file upload endpoint to ChatController and update application configuration for multipart support

--- back/src/main/java/org/maxbot/back/controller/ChatController.java
+++ back/src/main/java/org/maxbot/back/controller/ChatController.java
@@ -5,13 +5,22 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 public class ChatController {
@@ -20,8 +29,10 @@ public class ChatController {
     private String promptSystemTemplate;
 
     private final ChatClient chatClient;
+    private final VectorStore vectorStore;
 
     public ChatController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
@@ -38,5 +49,18 @@ public class ChatController {
                 .user(userInput)
                 .stream()
                 .content();
+    }
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<String> loadToRAG(@RequestParam MultipartFile file) {
+        PagePdfDocumentReader pdfDocumentReader = new PagePdfDocumentReader(file.getResource());
+        List<Document> documents = pdfDocumentReader.read();
+
+        TextSplitter textSplitter = new TokenTextSplitter();
+        List<Document> chunks = textSplitter.apply(documents);
+
+        vectorStore.add(chunks);
+
+        return ResponseEntity.ok().body("File uploaded successfully");
     }
 }
