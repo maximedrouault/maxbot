76bca594204b23d4713ab5ce5cf0870129593441
maximedrouault <maximedrouault@gmail.com>
2025-10-10 14:17:42 +0200

    refactor: integrate Pinecone vector store and update ChatController for new advisor

--- back/src/main/java/org/maxbot/back/controller/ChatController.java
+++ back/src/main/java/org/maxbot/back/controller/ChatController.java
@@ -3,7 +3,9 @@ package org.maxbot.back.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +16,25 @@ import reactor.core.publisher.Flux;
 @RestController
 public class ChatController {
 
+    @Value("${PROMPT_SYSTEM_TEMPLATE}")
+    private String promptSystemTemplate;
+
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
     }
-
-    @Value("${SYSTEM_TEMPLATE}")
-    private String systemTemplate;
 
 
     @GetMapping(value = "/chat", produces = MediaType.TEXT_PLAIN_VALUE)
     public Flux<String> chat(@RequestParam String userInput) {
 
         return this.chatClient.prompt()
-                .system(systemTemplate)
+                .system(promptSystemTemplate)
                 .user(userInput)
                 .stream()
                 .content();
