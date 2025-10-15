9c29a18c8e7ea3d0cc1d8a5b69f31d5c77ee5fce
maximedrouault <maximedrouault@gmail.com>
2025-10-15 16:26:18 +0200

    refactor: update ChatService to use session scope and initialize conversation ID

--- back/src/main/java/org/maxbot/back/service/ChatService.java
+++ back/src/main/java/org/maxbot/back/service/ChatService.java
@@ -2,33 +2,38 @@ package org.maxbot.back.service;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 import reactor.core.publisher.Flux;
 
+import java.util.UUID;
+
 @Service
+@SessionScope
 public class ChatService {
 
     @Value("${PROMPT_SYSTEM_TEMPLATE}")
     private String promptSystemTemplate;
 
     private final ChatClient chatClient;
+    private final UUID conversationId;
 
     public ChatService(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder
-                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .defaultAdvisors(QuestionAnswerAdvisor.builder(vectorStore).build())
                 .build();
+        this.conversationId = UUID.randomUUID();
     }
 
 
     public Flux<String> chatRequest(String userInput) {
         return this.chatClient.prompt()
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .system(promptSystemTemplate)
                 .user(userInput)
                 .stream()
