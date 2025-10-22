0b349876540ad776c2d24230b6860ea976fb1e87
maximedrouault <maximedrouault@gmail.com>
2025-10-22 16:51:07 +0200

    feat: update EmbeddingController to handle PDF uploads and add deleteByIds endpoint in EmbeddingService

--- back/src/main/java/org/maxbot/back/controller/EmbeddingController.java
+++ back/src/main/java/org/maxbot/back/controller/EmbeddingController.java
@@ -2,23 +2,32 @@ package org.maxbot.back.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.maxbot.back.service.EmbeddingService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/embedding")
 public class EmbeddingController {
 
     private final EmbeddingService embeddingService;
 
 
-    @PostMapping("/embedding/upload")
+    @PostMapping("/uploadPdf")
     public ResponseEntity<String> pdfToVectorStore(@RequestParam MultipartFile file) {
         embeddingService.pdfToVectorStore(file);
 
-        return ResponseEntity.ok().body("File uploaded successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @DeleteMapping("/deleteByIds")
+    public ResponseEntity<Void> deleteByIds(@RequestBody List<String> idList) {
+        embeddingService.deleteDocument(idList);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- back/src/main/java/org/maxbot/back/service/EmbeddingService.java
+++ back/src/main/java/org/maxbot/back/service/EmbeddingService.java
@@ -1,5 +1,6 @@
 package org.maxbot.back.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
@@ -11,13 +12,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmbeddingService {
 
     private final VectorStore vectorStore;
-
-    public EmbeddingService(VectorStore vectorStore) {
-        this.vectorStore = vectorStore;
-    }
 
 
     public void pdfToVectorStore(MultipartFile file) {
@@ -27,6 +25,14 @@ public class EmbeddingService {
             chunksToVectorStore(chunks);
         } catch (Exception e) {
             throw new RuntimeException("Failed to process PDF file to Vector store", e);
+        }
+    }
+
+    public void deleteDocument(List<String> idList) {
+        try {
+            vectorStore.delete(idList);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete documents from Vector store", e);
         }
     }
 
