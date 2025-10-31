ab2db2285d79600f93fbf0a7a74fd44160564382
maximedrouault <maximedrouault@gmail.com>
2025-10-31 18:46:13 +0100

    feat: implement global exception handler for rate limit violations

---
+++ back/src/main/java/org/maxbot/back/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.maxbot.back.exception;
+
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RequestNotPermitted.class)
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    public void handleRateLimitException() {}
+}
